6779af7bb7ea1e7ccbe203e516182edf09a141bd
Isaac M <Isaac M@192.168.1.8>
2014-02-01 22:09:10 +0000

    test commint

---
+++ admin/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <h1>Admin</h1>
+        <?php
+        echo "test";
+        ?>
+    </body>
+</html>
